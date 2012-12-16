e64d229f878bc5a82b841219f9bb35570661f71a
Peter Balazs <frikille@gmail.com>
2012-12-16 00:34:23 +0000

    String.format, Users wrapper, Repositories wrapper

--- src/module/Repositories.js
+++ src/module/Repositories.js
@@ -3,10 +3,10 @@
 	var that = {},
 		apiUrls = {
 			ownRepositories = '/user/repos',
-			getRepository = '/repos/:owner/:repo',
-			listContributors = '/repos/:owner/:repo/contributors',
-			listTags = '/repos/:owner/:repo/tags',
-			listBranches = '/repos/:owner/:repo/branches'
+			getRepository = '/repos/{0}/{1}',
+			listContributors = '/repos/{0}/{1}/contributors',
+			listTags = '/repos/{0}/{1}/tags',
+			listBranches = '/repos/{0}/{1}/branches'
 		},
 		apiMethods = {
 			ownRepositories = 'GET',
@@ -15,6 +15,82 @@
 			listTags = 'GET',
 			listBranches = 'GET'
 		};
+
+	that.getUserRepositories = function (options) {
+		var options = options || {};
+
+		if (!GitHubWrapper.checkAuthParams()) {
+			throw new Error('This method requires authorization, but no username and/or password in the options.');
+		}
+
+		options.auth == true;
+
+		options.method = apiMethods['ownRepositories'];
+		options.apiUrl = apiUrls['ownRepositories'];
+
+		GitHubWrapper.callApi(options);
+	};
+
+	that.getRepository = function (options) {
+		var options = options || {},
+			user = options.user,
+			repo = options.repo;
+
+		if (repo === undefined || user === undefined) {
+			throw new Error('This method requires user and repo, but no user and/or repo in the options.');
+		}
+
+		options.method = apiMethods['getRepository'];
+		options.apiUrl = apiUrls['getRepository'].format(user, repo);
+
+		GitHubWrapper.callApi(options);
+	};
+
+	that.listRepositoryContributors = function (options) {
+		var options = options || {},
+			user = options.user,
+			repo = options.repo;
+
+		if (repo === undefined || user === undefined) {
+			throw new Error('This method requires user and repo, but no user and/or repo in the options.');
+		}
+
+		options.method = apiMethods['listContributors'].format(user, repo);
+
+		GitHubWrapper.callApi(options);
+
+	};
+
+	that.listRepositoryTags = function (options) {
+		var options = options || {},
+			user = options.user,
+			repo = options.repo;
+
+		if (repo === undefined || user === undefined) {
+			throw new Error('This method requires user and repo, but no user and/or repo in the options.');
+		}
+
+		options.method = apiMethods['listTags'].format(user, repo);
+
+		GitHubWrapper.callApi(options);
+
+	};
+
+	that.listRepositoryBranches = function (options) {
+		var options = options || {},
+			user = options.user,
+			repo = options.repo;
+
+		if (repo === undefined || user === undefined) {
+			throw new Error('This method requires user and repo, but no user and/or repo in the options.');
+		}
+
+		options.method = apiMethods['listBranches'];
+		options.apiUrl = apiUrls['listBranches'].format(user, repo);
+
+		GitHubWrapper.callApi(options);
+
+	};
 
 	namespace.Repositories = that;
 
--- src/module/Users.js
+++ src/module/Users.js
@@ -12,7 +12,34 @@
 			updateAuthenticatedUser = 'PATCH'
 		};
 
-	that.getAuthenticatedUserData = function () {
+	that.getAuthenticatedUserData = function (options) {
+		var options = options || {};
+
+		if (!GitHubWrapper.checkAuthParams()) {
+			throw new Error('This method requires authorization, but no username and/or password in the options.');
+		}
+
+		options.auth == true;
+
+		options.method = apiMethods['getAuthenticatedUser'];
+		options.apiUrl = apiUrls['getAuthenticatedUser'];
+
+		GitHubWrapper.callApi(options);
+	};
+
+	that.updateAuthenticatedUser = function (options) {
+		var options = options || {};
+
+		if (!GitHubWrapper.checkAuthParams()) {
+			throw new Error('This method requires authorization, but no username and/or password in the options.');
+		}
+
+		options.auth == true;
+
+		options.method = apiMethods['updateAuthenticatedUser'];
+		options.apiUrl = apiUrls['updateAuthenticatedUser'];
+
+		GitHubWrapper.callApi(options);
 	}
 
 	namespace.Users = that;
