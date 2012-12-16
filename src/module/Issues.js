9ea81321cd69e7926eaf2588462f777c1712020c
Peter Balazs <frikille@gmail.com>
2012-12-16 00:50:18 +0000

    Issues wrapper API urls

--- src/module/Issues.js
+++ src/module/Issues.js
@@ -1,8 +1,52 @@
 (function (namespace, undefined) {
 
 	var that = {},
-		apiUrls = {},
-		apiMethods = {};
+		apiUrls = {
+			listUserAllIssues : '/issues',
+			listUserIssuesOfOwnedAndMemberRepositories : '/user/issues/',
+			getIssue : '/repos/{0}/{1}/issues/{2}',
+			createIssue : '/repos/{0}/{1}/issues',
+			editIssue : '/repos/{0}/{1}/issues/{2}',
+			listAssignees : '/repos/{0}/{1}/assignees',
+			listCommentsOnIssue : '/repos/{0}/{1}/issues/{2}/comments',
+			getComment : '/repos/{0}/{1}/issues/comments/{2}',
+			createComment : '/repos/{0}/{1}/issues/{2}/comments'
+			editComment : '/repos/{0}/{1}/issues/comments/{2}',
+			deleteComment : '/repos/{0}/{1}/issues/comments/{2}',
+			listLabels : '/repos/{0}/{1}/labels',
+			createLabel : '/repos/{0}/{1}/labels/',
+			listLabelsOnIssue : '/repos/{0}/{1}/issues/{2}/labels',
+			addLabelToIssue : '/repos/{0}/{1}/issues/{2}/labels',
+			removeLabelFromIssue : '/repos/{0}/{1}/issues/{2}/labels/:name',
+			replaceLabelsOnIssue : '/repos/{0}/{1}/issues/{2}/labels'
+			listMilestones : '/repos/{0}/{1}/milestones',
+			getMilestone : '/repos/{0}/{1}/milestones/{2}',
+			createMilestone : '/repos/{0}/{1}/milestones',
+			updateMilestone : '/repos/{0}/{1}/milestones/{2}'
+		},
+		apiMethods = {
+			listUserAllIssues : 'GET',
+			listUserIssuesOfOwnedAndMemberRepositories : 'GET',
+			getIssue : 'GET',
+			createIssue : 'POST',
+			editIssue : 'PATCH',
+			listAssignees : 'GET',
+			listCommentsOnIssue : 'GET',
+			getComment : 'GET',
+			createComment : 'POST',
+			editComment : 'PATCH',
+			deleteComment : 'DELETE',
+			listLabels : 'GET',
+			createLabel : 'POST',
+			listLabelsOnIssue : 'GET',
+			addLabelToIssue : 'POST',
+			removeLabelFromIssue : 'DELETE',
+			replaceLabelsOnIssue : 'PUT',
+			listMilestones : 'GET',
+			getMilestone : 'GET',
+			createMilestone : 'POST',
+			updateMilestone : 'PATCH'
+		};
 
 	namespace.Issues = that;
 
